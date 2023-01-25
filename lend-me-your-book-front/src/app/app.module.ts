d90162966769e4d9eb7f37f5c06f5f7647b81612
Ayoub Ltifi <ayoub.ltifi1@gmail.com>
2023-01-25 11:15:45 +0100

    create a header component and fix some layout bugs

--- lend-me-your-book-front/src/app/app.module.ts
+++ lend-me-your-book-front/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { DonorComponent } from './components/donor/donor.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
-    DonorComponent
+    DonorComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
