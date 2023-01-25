e527f73c8619afd3989d0c05b5657fad532a6251
Ayoub Ltifi <ayoub.ltifi1@gmail.com>
2023-01-25 10:14:22 +0100

    create the welcome page component and the donor component

--- lend-me-your-book-front/src/app/app.module.ts
+++ lend-me-your-book-front/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { DonorComponent } from './components/donor/donor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    DonorComponent
   ],
   imports: [
     BrowserModule
